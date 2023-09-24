4d937136c45bbc7c08321617824765e53ca7ddd5
George vila <gvila92@gmail.com>
2023-09-24 08:42:32 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+function writeCards(names, occasion) {
+    const thankYouMessages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      const name = names[i];
+      const message = `Thank you, ${name}, for the wonderful ${occasion} gift!`;
+      thankYouMessages.push(message);
+    }
+  
+    return thankYouMessages;
+  }
+  
+  const messages = writeCards(["Charlie", "Samip", "Ali"], "surprise");
+  console.log(messages);
+  
+  function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  
+  countDown(5);
